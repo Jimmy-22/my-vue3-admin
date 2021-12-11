fff3b2b7f9246af26d4de59ce1771b9a7b3800b3
ferrytsui <ferrytsui@163.com>
2021-12-11 21:29:41 +0800

    把获取到的token存到vuex和localstorage

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,10 +1,19 @@
 import { login } from '@/api/sys.js'
 import md5 from 'md5'
+import { setItem, getItem } from '../../utils/storage'
+import { TOKEN } from '../../constant'
 
 export default {
   namespaced: true,
-  state: () => ({}),
-  mutations: {},
+  state: () => ({
+    token: getItem(TOKEN) || ''
+  }),
+  mutations: {
+    setToken(state, token) {
+      state.token = token
+      setItem(TOKEN, token)
+    }
+  },
   actions: {
     login(context, userInfo) {
       const { username, password } = userInfo
@@ -14,6 +23,8 @@ export default {
           password: md5(password)
         })
           .then((data) => {
+            // console.log(data)
+            this.commit('user/setToken', data.data.data.token)
             resolve()
           })
           .catch((err) => {
