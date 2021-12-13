8bdea0168991c6ee010ea1868a2d07669cdb710b
xujin <ferrytsui@163.com>
2021-12-13 10:18:20 +0800

    layout基本布局

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,3 +1,45 @@
 <template>
-  <div>layout页面</div>
+  <div class="app-wrapper">
+    <sidebar
+      id="guide-sidebar"
+      class="sidebar-container"
+      :style="{ backgroundColor: variables.menuBg }"
+    />
+    <div class="main-container">
+      <div class="fix-header">
+        <navbar />
+      </div>
+      <app-main />
+    </div>
+  </div>
 </template>
+
+<script setup>
+import variables from '@/styles/variables.scss'
+import Navbar from './components/Navbar'
+import Sidebar from './components/Sidebar'
+import AppMain from './components/AppMain'
+</script>
+
+<style lang="scss" scoped>
+@import '~@/styles/mixin.scss';
+@import '~@/styles/variables.scss';
+
+.app-wrapper {
+  @include clearfix;
+  @include relative;
+}
+
+.fixed-header {
+  position: fixed;
+  top: 0;
+  right: 0;
+  z-index: 9;
+  width: calc(100% - #{$sideBarWidth});
+  transition: width #{$sideBarDuration};
+}
+
+.hideSidebar .fixed-header {
+  width: calc(100% - #{$hideSideBarWidth});
+}
+</style>
